536d7dc5dc1e1ccfa30da96a6e6c8de106f75808
Rhea-Braithwaite <rfb7689@nyu.edu>
2024-02-26 14:46:44 +0400

    Fixed error to make test fail

--- src/main/java/nyuad/cs3216/exceptions/DivideByZeroException.java
+++ src/main/java/nyuad/cs3216/exceptions/DivideByZeroException.java
@@ -4,6 +4,6 @@ package nyuad.cs3216.exceptions;
 public class DivideByZeroException extends Exception{
     
     public DivideByZeroException(String errorMessage) {
-        super(errorMessage)
+        super(errorMessage);
     }
 }
